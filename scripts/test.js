a11af5a80d6190b7cb889b989697857b05126a7b
JerinPaulS <jerinsprograms@gmail.com>
2022-04-07 10:22:09 +0530

    Basics of ES6 and Typescript

---
+++ scripts/test.js
@@ -0,0 +1,12 @@
+"use strict";
+function greetPerson(name) {
+    if (name == "Jerin") {
+        var greet = "Hello Jerin!";
+    }
+    else {
+        var greet = "Hello Somebody!";
+    }
+    console.log(greet);
+}
+greetPerson("Jerin");
+greetPerson("Paul");
---
+++ test.ts
@@ -0,0 +1,13 @@
+function greetPerson(name){
+    var greet;
+    if(name=="Jerin"){
+        greet="Hello Jerin!";
+    }
+    else{
+        greet="Hello Somebody!";
+    }
+    console.log(greet);
+}
+
+greetPerson("Jerin");
+greetPerson("Paul")
